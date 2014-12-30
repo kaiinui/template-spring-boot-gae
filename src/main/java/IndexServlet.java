bf04053b1f009ecb255617c8ea4cd90fe05974a4
Kai INUI <lied.der.optik@gmail.com>
2014-12-30 22:09:37 +0900

    AppEngine runs.

---
+++ src/main/java/IndexServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by kaiinui on 2014/12/30.
+ */
+public class IndexServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello World Yay!");
+    }
+}
